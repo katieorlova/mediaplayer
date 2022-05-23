37a04fd23b380a7ddfaaa8e312a012bd013977b6
VaVasenyova <91275430+VaVasenyova@users.noreply.github.com>
2022-05-23 21:24:03 +0300

    Add files via upload

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,23 +1,36 @@
-package com.example.mediaplayer;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+package com.example.demo;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("interface1.fxml")));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(arg0 -> {
+
+            Platform.exit();
+            System.exit(0);
+        });
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- HelloController.java
+++ HelloController.java
@@ -1,14 +1,192 @@
-package com.example.mediaplayer;
+package com.example.demo;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Pane pane;
+    @FXML
+    public Label songLabel;
+    @FXML
+    public Button playButton, pauseButton, resetButton, previousButton, nextButton;
+    @FXML
+    public Slider volumeSlider;
+    @FXML
+    private ProgressBar songProgressBar;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    private File directory;
+    private File[] files;
+
+    private ArrayList<File> songs;
+
+    private int songNumber;
+
+    private Timer timer;
+    private TimerTask task;
+
+    private boolean running;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+        songs = new ArrayList<File>();
+
+        directory = new File("C:\\Users\\Лера\\IdeaProjects\\demo\\music");
+
+        files = directory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                songs.add(file);
+            }
+        }
+
+        media = new Media(songs.get(songNumber).toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+
+        songLabel.setText(songs.get(songNumber).getName());
+
+
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+
+                mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+            }
+        });
+
+        songProgressBar.setStyle("-fx-accent: #00FF00;");
+    }
+
+    public void playMedia(ActionEvent actionEvent) {
+        beginTimer();
+        mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+        mediaPlayer.play();
+    }
+
+    public void pauseMedia(ActionEvent actionEvent) {
+        cancelTimer();
+        mediaPlayer.pause();
+    }
+
+    public void resetMedia(ActionEvent actionEvent) {
+        songProgressBar.setProgress(0);
+        mediaPlayer.seek(Duration.seconds(0));
+    }
+
+    public void previousMedia(ActionEvent actionEvent) {
+        if (songNumber > 0) {
+            songNumber = songNumber - 1;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+
+            songLabel.setText(songs.get(songNumber).getName());
+
+            playMedia();
+        } else {
+
+            songNumber = songs.size() - 1;
+            mediaPlayer.stop();
+            if (running) {
+                cancelTimer();
+            }
+
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+
+            songLabel.setText(songs.get(songNumber).getName());
+
+            playMedia();
+        }
+    }
+
+    private void playMedia() {
+    }
+
+    public void nextMedia(ActionEvent actionEvent) {
+        if (songNumber < songs.size() - 1) {
+
+            songNumber = songNumber + 1;
+
+            mediaPlayer.stop();
+
+            if (running) {
+
+                cancelTimer();
+            }
+
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+
+            songLabel.setText(songs.get(songNumber).getName());
+
+            playMedia();
+        } else {
+
+            songNumber = 0;
+
+            mediaPlayer.stop();
+
+            media = new Media(songs.get(songNumber).toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+
+            songLabel.setText(songs.get(songNumber).getName());
+
+            playMedia();
+        }
+    }
+
+    public void beginTimer() {
+
+        timer = new Timer();
+
+        task = new TimerTask() {
+
+            public void run() {
+
+                running = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
+                songProgressBar.setProgress(current / end);
+
+                if (current / end == 1) {
+
+                    cancelTimer();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 1000);
+    }
+
+    public void cancelTimer() {
+
+        running = false;
+        timer.cancel();
     }
 }
